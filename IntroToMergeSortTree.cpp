58e3122035385d9f9fcb0b8fb784824d6c967ba3
shxxtzcoder <115549971+shxxtzcoder@users.noreply.github.com>
2023-12-21 16:59:07 +0530

    Add files via upload

---
+++ IntroToMergeSortTree.cpp
@@ -0,0 +1,29 @@
+/*
+Let's see some of the problems where segment trees may not be helpful.
+(1)Given an array of size N and Q queries of the form L,R,K, find number of elements in the range L to R which are strictly smaller than K.
+(2)Given an array of size N and Q queries of the form L R,find number of unique elements in the range L to R.
+-->We can solve this using Mo's algo as well,but that would require all queries to be offline(already known),we want a 'good' algo for online queries as well.
+
+We are gonna solve the following probs:
+(1)Coloring Trees(Hackerrank)
+(2)Subtree Minimum Query(Codeforces)
+[requires 'Flattening of Trees']
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define mod 1000000007
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define accDeci cout<<fixed;cout<<setprecision(10);
+
+int32_t main()
+{
+    fastIO;accDeci;
+    int t;cin>>t;
+    while(t--)
+    {
+
+    }
+    return 0;
+}
